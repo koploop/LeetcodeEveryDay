93e3661b84e5f4c423d1f094a6c5381d486799d4
koploop <lvhan0415@126.com>
2018-04-13 15:21:43 +0800

    Udpate 002

--- leetcode-002.playground/Contents.swift
+++ leetcode-002.playground/Contents.swift
@@ -9,17 +9,48 @@ var str = "Hello, playground"
  */
 
 class ListNode {
-    var val: Int
-    var next: ListNode?
-    init(value: Int) {
-        val = value
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
     }
 }
 
 class Solution {
-    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        var pre = ListNode(value: 0)
+    public static func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        var _l1 = l1, _l2 = l2
+        var current = ListNode(0)
+        let pCurrent = current
         var carry = 0
+        while _l1 != nil || _l2 != nil || carry != 0 {
+            let sum = (_l1 != nil ? _l1!.val : 0) + (_l2 != nil ? _l2!.val : 0) + carry
+            carry = sum / 10
+            current.next = ListNode(sum % 10)
+            current = current.next!
+            
+            if let list1 = _l1 {
+                _l1 = list1.next
+            }
+            if let list2 = _l2 {
+                _l2 = list2.next
+            }
+            print(current.val)
+        }
+        return pCurrent.next
+    }
+    
+    public static func test() {
+        let list1 = ListNode(2)
+        list1.next = ListNode(4)
+        list1.next?.next = ListNode(3)
         
+        let list2 = ListNode(5)
+        list2.next = ListNode(6)
+        list2.next?.next = ListNode(4)
+        
+        _ = addTwoNumbers(list1, list2)
     }
 }
+
+Solution.test()
